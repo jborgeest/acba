72993bfde0366e378966216b098074a1d25f1466
Alan Yeung <alan@creativebits.com.au>
2014-05-04 07:11:11 +1000

    made an autogen menu, little css fix for menu pos

--- html/inc/init.php
+++ html/inc/init.php
@@ -8,22 +8,21 @@ $REQUEST_LINK = !empty($_GET['page']) ? $_GET['page'] : '';
 $PAGE_ID = Page::resolvePageId($REQUEST_LINK);
 
 $is_home = $PAGE_ID == 'home';
+$nav_menus = Menu::generate();
 
 /* ============================ */
 
-echo '<pre>';
-$menus = Menu::generate();
-foreach ($menus as $collation => $submenus){
-	echo "$collation has: ";
-	foreach ($submenus as $submenu){
-		echo "{$submenu->label()}[{$submenu->link()}], ";
-	}
-	echo PHP_EOL;
-}
-echo '</pre>';
+// echo '<pre>';
+// foreach ($menus as $collation => $submenus){
+	// echo "$collation has: ";
+	// foreach ($submenus as $submenu){
+		// echo "{$submenu->label()}[{$submenu->link()}], ";
+	// }
+	// echo PHP_EOL;
+// }
+// echo '</pre>';
 
 
 // # Go
 $page = new Page($PAGE_ID);
-
 include 'view/'.$page->filename();
